7b48c01eb70d6aec363c2611fef377a694d46ac2
RamonKea-inh <687187@student.inholland.nl>
2023-02-21 12:15:25 +0100

    admin login and validation

---
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href=
+    "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="login.css">
+    <title>Admin Page</title>
+</head>
+ 
+<body>
+
+</body>
+ 
+</html>
