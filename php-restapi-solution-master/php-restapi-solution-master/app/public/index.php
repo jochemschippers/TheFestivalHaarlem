abdf59f237aca079fefe9c2ee7d0155fadf95ae2
jochemschippers <jochemschippers@gmail.com>
2023-02-21 11:34:50 +0100

    fixes

--- php-restapi-solution-master/php-restapi-solution-master/app/public/index.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/public/index.php
@@ -3,17 +3,7 @@ require __DIR__ . '/../patternrouter.php';
 
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-$file = "<h1>test</h1>";
-echo filter_var($file,FILTER_UNSAFE_RAW);
+
 
 $router = new PatternRouter();
 $router->route($uri);
-
-require_once("dbconfig.php");
-
-try {
-    $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $options);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $error) {
-        echo "Connection failed: " . $error->getMessage();
-}
