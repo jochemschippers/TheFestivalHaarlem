403ad54fcbd04200dcd31746caf1d71d04a74107
TayamEJ <89915786+Tayam0nline@users.noreply.github.com>
2023-03-02 12:03:05 +0100

    navbar added
    missing functionality to connect with db and small css

---
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/navbar.php
@@ -0,0 +1,22 @@
+
+<html lang="en">
+<head>
+  <link href="/css/navbar.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+<body>
+    <nav class="navbar">
+
+       <div class="navbar-container container">
+            <ul class="menu-items">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Jazz</a></li>
+                <li><a href="#">Yummy!</a></li>
+                <li><a href="#">A Stroll Through History</a></li>
+            </ul>
+         <a href="#"><img class="logo" src="https://i.ibb.co/9TR3YTC/Logo-small2.png"> </img></a>
+    </div>
+<i class="fas fa-shopping-cart fa-lg" style="color:black;"></i>
+    </nav>
+</body>
+</html>
