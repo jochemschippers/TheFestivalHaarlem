fd3735e860707e053beb96aa1ab688c0c3747d13
TayamEJ <89915786+Tayam0nline@users.noreply.github.com>
2023-03-07 16:06:34 +0100

    added slider

--- php-restapi-solution-master/php-restapi-solution-master/app/views/jazz/index.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/jazz/index.php
@@ -35,8 +35,39 @@
             </div>
         </div>
     </div>
-    <div class="container special" style="height: 20vw; background-color: #F9C9C9">
-        
+    <div class="container special" style="height: 25vw; background-color: #F9C9C9">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="/image/jazz/candyAndHansDulfer.png">
+          <h1></h1>
+        </div>
+        <div class="carousel-item">
+        <img src="/image/jazz/candyAndHansDulfer.png">
+        </div>
+        <div class="carousel-item">
+        <img src="/image/jazz/candyAndHansDulfer.png">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+ 
     </div>
 
     </body>
