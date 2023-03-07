b0f16a91b8927523f6c0fc1f28f1944a41c34a53
TayamEJ <89915786+Tayam0nline@users.noreply.github.com>
2023-03-07 12:01:59 +0100

    added jazz page
    working on styling page correctly

---
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/jazz/index.php
@@ -0,0 +1,44 @@
+<!-- ----- HIER MOET HEADER/ NAV BAR ----- -->
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title> <!-- <<<<<<< HIER ANDERE TITLE -->
+
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link href="../css/template.css" rel="stylesheet">
+        <link href="../css/jazz/jazz.css" rel="stylesheet">
+
+
+    </head>
+    <body>
+        
+    <div class="border-box">
+        <div class="container" id="titleContainer">
+            <h1>Haarlem Jazz</h1> 
+        </div>
+    </div>
+    <div class="container landingPageContainer">
+    <div class="row">
+    <div class="col-md-7" id="titleText"> 
+        <h2>Learn about our <strong>artists<strong></h2>  
+        <p>The Festival Jazz is a premier event. Showcasing the best in local and international jazz talent, in partnership with Haarlem Jazz, the festival’s jazz events provide a vibrant and lively atmosphere for music fans to come together and enjoy the sounds of the genre. 
+
+Read below more about the artists, schedule and locations!</p>     
+    </div>
+    <div class="col-md-5">
+    </div>    
+  </div>
+        
+    </div>
+
+
+    </body>
+</html>
+
+<?php 
+include __DIR__ . '/../footer.php';
+?>
