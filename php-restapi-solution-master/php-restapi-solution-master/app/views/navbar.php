3a054d8aceb86c7518453adc690691ee0ae19ec9
TayamEJ <89915786+Tayam0nline@users.noreply.github.com>
2023-03-11 12:20:28 +0100

    completion of navbar
    includes javacript in order to select correct item
    finalisation to css of navbar

--- php-restapi-solution-master/php-restapi-solution-master/app/views/navbar.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/navbar.php
@@ -5,34 +5,53 @@ $events = $eventcontroller->getEvents();
 ?>
 
 <html lang="en">
+
 <head>
   <link href="/css/navbar.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  </head>
-  
-  <header class="header-container">
-    <div class="logo">
-        <img class="logoImage" src="https://i.ibb.co/9TR3YTC/Logo-small2.png" alt="logo festival"> </img>
-    </div>
-    <nav class="navbar">
-      <div class="nav-item"><a href="/">Home</a></div>
-      <?php 
-              try{
-                foreach ($events as $event) {
-                    echo '<div class="nav-item"><a href="/' . str_replace(' ', '-', $event->getEventTitle()) . '">' . $event->getEventTitle()  . '</a></div>';
-                  }
-              }
-              catch(error $e)
-              {
-                echo "something went wrong while loading the navigation! Please try again later";
-                echo "<script>console.log('Debug Objects: " . $e->getMessage() . "' );</script>";
-              }
-              
-              ?>
-    </nav>
-    <div class="user-options">
-      <div class="option"><i class="fa fa-user fa-2x"></i></div>
-      <div class="option"><i class="fa fa-shopping-cart fa-2x"></i></div>
-    </div>
-  </header>
+</head>
+
+<header class="header-container">
+  <div class="logo">
+    <img class="logoImage" src="https://i.ibb.co/9TR3YTC/Logo-small2.png" alt="logo festival"> </img>
+  </div>
+  <nav class="navbar" id="navbar">
+    <span class="nav-item"><a href="/">Home</a></span>
+    <?php
+    try {
+      foreach ($events as $event) {
+        echo '<span class="nav-item"><a href="/' . str_replace(' ', '-', $event->getEventTitle()) . '">' . $event->getEventTitle()  . '</a></span>';
+      }
+    } catch (error $e) {
+      echo "something went wrong while loading the navigation! Please try again later";
+      echo "<script>console.log('Debug Objects: " . $e->getMessage() . "' );</script>";
+    }
+
+    ?>
+
+    <script>
+      const params = window.location.pathname.split("/");
+      const navbar = document.getElementById("navbar");
+      const links = navbar.getElementsByTagName("span");
+      console.log(links);
+      console.log(params[1]);
+      if(params[1] === "" || params[1] === null){
+        links[0].classList.add("selected");
+      }
+      else{
+        for (let i = 0; i < links.length; i++) {
+        if (links[i].textContent === params[1]) {
+          links[i].classList.add("selected");
+        }
+      }
+      }
+     
+    </script>
+  </nav>
+  <div class="user-options">
+    <div class="option"><i class="fa fa-user fa-2x"></i></div>
+    <div class="option"><i class="fa fa-shopping-cart fa-2x"></i></div>
+  </div>
+</header>
+
 </html>
