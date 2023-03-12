3e8b36dc6fce82c6ae192934429fa35a3ba8551c
TayamEJ <89915786+Tayam0nline@users.noreply.github.com>
2023-03-12 18:50:01 +0100

    Update jazzrepository.php

--- php-restapi-solution-master/php-restapi-solution-master/app/repositories/jazzrepository.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/repositories/jazzrepository.php
@@ -8,39 +8,13 @@ class JazzRepository extends Repository
     function getAllArtists()
     {
         try {
-            $stmt = $this->connection->prepare("SELECT * FROM landmark");
+            $stmt = $this->connection->prepare("SELECT artistID,`description`,`image`,`name` FROM JazzArtists");
             $stmt->execute();
 
-            $stmt->setFetchMode(PDO::FETCH_CLASS, 'Landmark');
+            $stmt->setFetchMode(PDO::FETCH_CLASS, 'jazzArtist');
             return $stmt->fetchAll();
         } catch (PDOException $e) {
             echo $e;
         }
     }
-
-    // function getAll()
-    // {
-    //     try {
-    //         $stmt = $this->connection->prepare("SELECT * FROM festivalEvent");
-    //         $stmt->execute();
-
-    //         $stmt->setFetchMode(PDO::FETCH_CLASS, 'FestivalEvent');
-    //         $festivalevents = $stmt->fetchAll();
-
-    //         return $festivalevents;
-    //     } catch (PDOException $e) {
-    //         echo $e;
-    //     }
-    // }
-
-    // function insert($article) {
-    //     try {
-    //         $stmt = $this->connection->prepare("INSERT into article (title, content, author, posted_at) VALUES (?,?,?, NOW())");
-            
-    //         $stmt->execute([$article->getTitle(), $article->getContent(), $article->getAuthor()]);
-
-    //     } catch (PDOException $e) {
-    //         echo $e;
-    //     }
-    // }
 }
