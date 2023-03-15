e5d625ddc5b997eaa64d6b9cc8bb3242e5aa7ace
jochemschippers <jochemschippers@gmail.com>
2023-03-15 14:08:29 +0100

    fixes for payment

--- php-restapi-solution-master/php-restapi-solution-master/app/views/paymentpage/login.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/paymentpage/login.php
@@ -36,7 +36,7 @@ $grand_total = 0;
 <img id="background" src="\image\Payment\overview\backgroundpayment.png"  >
 
 
-<div class="container">
+<div class="container" id="progressContainer">
     <div class="card">
         <div class="row d-flex justify-content-between px-3 top">
             <div class="d-flex">
@@ -49,32 +49,32 @@ $grand_total = 0;
             <div class="col-12">
             <ul id="progressbar" class="text-center">
                 <li class="active step0"></li>
-                <li class="active step0"></li>
+                <li class="step0"></li>
                 <li class="step0"></li>
                 <li class="step0"></li>
             </ul>
             </div>
         </div>
-        <div class="row justify-content-between top">
-            <div class="row d-flex icon-content">
+        <div class="row justify-content-between top" id="row-width">
+            <div class="row d-flex icon-content"  id="flex">
                 <img class="icon" src="https://i.imgur.com/9nnc9Et.png">
                 <div class="d-flex flex-column">
                     <p class="font-weight-bold">Review<br>Tickets</p>
                 </div>
             </div>
-            <div class="row d-flex icon-content">
+            <div class="row d-flex icon-content"  id="flex">
                 <img class="icon" src="http://cdn.onlinewebfonts.com/svg/img_230731.png">
                 <div class="d-flex flex-column">
                     <p class="font-weight-bold">Login</p>
                 </div>
             </div>
-            <div class="row d-flex icon-content">
+            <div class="row d-flex icon-content"  id="flex">
                 <img class="icon" src="https://cdn3.iconfinder.com/data/icons/online-shopping-line-flash-deals/512/Cash_on_delivery-512.png">
                 <div class="d-flex flex-column">
                     <p class="font-weight-bold">Payment<br>Information</p>
                 </div>
             </div>
-            <div class="row d-flex icon-content">
+            <div class="row d-flex icon-content"  id="flex">
                 <img class="icon" src="https://th.bing.com/th/id/R.c4f226e94861c395866a9b2c3bacb482?rik=aIhV%2fV8GkQcsFw&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_432058.png&ehk=1NipJCsVQFhbh12bsO25ElaQQJYGZRsglyiHmv4%2bXjE%3d&risl=&pid=ImgRaw&r=0">
                 <div class="d-flex flex-column">
                     <p class="font-weight-bold">Recieve<br>Tickets</p>
@@ -83,9 +83,11 @@ $grand_total = 0;
         </div>
     </div>
 </div>
-<div class="container">
-    
+<div class="container" id="container">
+    <p id="message">You are already logged in.</p>
+    <a href="/paymentpage/payment" class="btn btn-primary" id="continueButton">Continue</a>
 </div>
+    
 </body> 
 </html>
 <?php
