70eb88dbaf55fead934fd24e015212df94e44075
Ramon Kea <687187@student.inholland.nl>
2023-03-18 22:06:24 +0100

    Update index.php

--- php-restapi-solution-master/php-restapi-solution-master/app/views/admin/index.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/admin/index.php
@@ -1,39 +1,6 @@
 <?php
 include __DIR__ . '/../navbar.php';
 
-// Handle form submissions
-if (isset($_POST['submit'])) {
-  switch ($_POST['submit']) {
-    case 'Create':
-      $title = $_POST['title'];
-      $description = $_POST['description'];
-      $image = $_POST['image'];
-
-      $controller->createLandmark($title, $description, $image);
-
-      break;
-    case 'Update':
-      $landmarkID = $_POST['landmarkID'];
-      $title = $_POST['title'];
-      $description = $_POST['description'];
-      $image = $_POST['image'];
-
-      $controller->updateLandmark($landmarkID, $title, $description, $image);
-
-      break;
-  }
-}
-
-// Display landmarks and form
-// $controller->displayLandmarks();
-// $controller->displayCreateForm();
-if (isset($_GET['action']) && $_GET['action'] == 'edit') {
-  $landmarkID = $_GET['landmarkID'];
-  $landmark = $controller->getLandmark($landmarkID);
-  if ($landmark) {
-    $controller->displayEditForm($landmarkID, $landmark->getTitle(), $landmark->getDescription(), $landmark->getImage());
-  }
-}
 ?>
 
 <!DOCTYPE html>
@@ -51,28 +18,9 @@ if (isset($_GET['action']) && $_GET['action'] == 'edit') {
     <h1>Landmarks</h1>
 
     <div id="landmarks-table">
-        <?php //$controller->displayLandmarks();
-            // foreach ($model as $landmark) 
-            // {
-            //     echo "<div class='col-md-4'>";
-            //     echo "<div class='card mb-4 shadow-sm'>";
-            //     echo "<img class='card-img-top' src='images/" . $landmark->getImage() . "' alt='Card image cap'>";
-            //     echo "<div class='card-body'>";
-            //     echo "<h5 class='card-title'>" . $landmark->getTitle() . "</h5>";
-            //     echo "<p class='card-text'>" . $landmark->getDescription() . "</p>";
-            //     echo "<div class='d-flex justify-content-between align-items-center'>";
-            //     echo "<div class='btn-group'>";
-            //     echo "<a href='index.php?controller=landmark&action=edit&landmarkID=" . $landmark->getLandmarkID() . "' class='btn btn-sm btn-outline-secondary'>Edit</a>";
-            //     echo "<a href='index.php?controller=landmark&action=delete&landmarkID=" . $landmark->getLandmarkID() . "' class='btn btn-sm btn-outline-secondary'>Delete</a>";
-            //     echo "</div>";
-            //     echo "</div>";
-            //     echo "</div>";
-            //     echo "</div>";
-            //     }
-        ?>
-    
         <?php
-        foreach ($model as $landmark)
+
+        foreach ($models as $landmark)
         {
             echo "<h2>" . $landmark->getTitle() . "</h2>";
             echo "<p>" . $landmark->getDescription() . "</p>";
@@ -80,45 +28,36 @@ if (isset($_GET['action']) && $_GET['action'] == 'edit') {
             // echo "<img src='images/" . $landmark->getImage() . "' alt='Landmark image'>";
         }
         ?>
+
     </div>
 
     <h2>Add Landmark</h2>
 
     <div id="landmark-form">
-        <?php $controller->displayCreateForm(); ?>
+        <form action="index.php?controller=landmark&action=create" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="title">Title</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="description">Description</label>
+                <input type="text" id="description" name="description">
+            </div>
+            <div class="mb-3">
+                <label for="image">Image</label>
+                <input type="text" id="image" name="image">
+            </div>
+            <button type="submit" class="btn btn-primary">Create</button>
     </div>
 
     <h2>Edit Landmark</h2>
 
     <div id="edit-landmark-form">
         <?php
-            if (isset($_GET['action']) && $_GET['action'] == 'edit') {
-                $landmarkID = $_GET['landmarkID'];
-                $landmark = $controller->getLandmark($landmarkID);
+            // edit landmark form
+            
+            
 
-                if ($landmark) {
-                    //$controller->displayEditForm($landmarkID);
-                    echo "<form action='index.php?controller=landmark&action=update' method='post' enctype='multipart/form-data'>";
-                    echo "<div class='mb-3'>";
-                    echo "<label for='title'>Title</label>";
-                    echo "<input type='text' class='form-control' id='title' name='title' value='" . $landmark->getTitle() . "'>";
-                    echo "</div>";
-                    echo "<div class='mb-3'>";
-                    echo "<label for='description'>Description</label>";
-                    echo "<input type='text' id='description' name='description' value='" . $landmark->getDescription() . "'>";
-                    echo "</div>";
-                    echo "<div class='mb-3'>";
-                    echo "<label for='image'>Image</label>";
-                    echo "<input type='text' id='image' name='image'>";
-                    echo "</div>";
-                    echo "<input type='text' name='landmarkID' value='" . $landmark->getLandmarkID() . "'>";
-                    echo "<button type='submit' class='btn btn-primary'>Update</button>";
-                    echo "</form>";
-                    
-                } else {
-                    echo "Landmark not found";
-                }
-            }
         ?>
     </div>
     <form action="historycontroller.php" method="post"></form>
