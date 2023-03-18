b319edf22d02fb27e0c4ede808ee955efdfef290
Ramon Kea <687187@student.inholland.nl>
2023-03-18 17:42:30 +0100

    updated some landmark crud code

--- php-restapi-solution-master/php-restapi-solution-master/app/services/historyservice.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/services/historyservice.php
@@ -5,40 +5,31 @@ require __DIR__ . '/../repositories/historyrepository.php';
 class HistoryService 
 {
     private $repository;
+
     function __construct()
     {
         $this->repository = new HistoryRepository();
     }
+
     public function insert($history) {
-        // retrieve data
-        $repository = new HistoryRepository();
-        $repository->insert($history);        
+        $this->repository->insert($history);        
     }
     
     public function getAll() {
         // retrieve data
-        $repository = new HistoryRepository();
-        $history = $repository->getAll();
-        return $history;
+        return $this->repository->getAll();
     }
 
     public function get($historyID) {
-        // retrieve data
-        $repository = new HistoryRepository();
-        $history = $repository->get($historyID);
-        return $history;
+        return $this->repository->get($historyID);
     }
 
     public function update($history) {
-        // retrieve data
-        $repository = new HistoryRepository();
-        $repository->update($history);
+        return $this->repository->update($history);
     }
 
     public function delete($historyID) {
-        // retrieve data
-        $repository = new HistoryRepository();
-        $repository->delete($historyID);
+        return $this->repository->delete($historyID);
     }
 }
 ?>
