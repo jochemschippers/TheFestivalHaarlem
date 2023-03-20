a1ea31238d5a969c5be409c2eb3805270ed1c374
Ramon Kea <687187@student.inholland.nl>
2023-03-20 16:38:37 +0100

    Update index.php

--- php-restapi-solution-master/php-restapi-solution-master/app/views/strollthroughhistory/index.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/strollthroughhistory/index.php
@@ -1,44 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-    <!-- <head>
-        <script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script>
-    </head> -->
-    <body>
-    <div class="layered">
-      <div class="background-Image">
-        <div class="border-box">
-          <div class="container" id="titleContainer">
-          <h1>A stroll through history</h1>
-        </div>
-      </div>
-      <div class="container landingPageContainer">
-        <div class="row">
-          <div class="col-md-7" id="titleText">
-          <h2><strong>Visit</strong> all the historic sights of Haarlem</h2>
-            <p>The historical inner city of Haarlem features lots of extraordinary monuments. 
-            Discover the many interesting and surprising monuments that this city makes unique through this tour. 
-            Our guides will take you along several monuments in the city of Haarlem as you can see below.
-            </p>
-          </div>
-          <div class="col-md-7">
-            <h4><strong>Check out our Schedule</strong></h4>
-            <button><strong>↓</strong></button>
-          </div>
-        </div>
+
+  <head>
+      <!-- <script src="https://cdn.ckeditor.com/ckeditor5/36.0.1/classic/ckeditor.js"></script> -->
+      <link href="../css/strollthroughhistory/strollthroughhistory.css" rel="stylesheet">
+  </head>
+  <body>
+  <div class="layered">
+    <div class="background-Image">
+      <div class="border-box">
+        <div class="container" id="titleContainer">
+        <h1>A stroll through history</h1>
       </div>
     </div>
-    <div class="embed-responsive embed-responsive-16by9">
-      <iframe
-      class="embed-responsive-item"
-      src="https://www.google.com/maps/d/u/0/embed?mid=1xMakWcCkWRc-eW4IGAR_zDG1UttbB1k&ehbc=2E312F"
-      title="Landmark Route">
-      </iframe>
+    <div class="container landingPageContainer">
+      <div class="row">
+        <div class="col-md-7" id="titleText">
+        <h2><strong>Visit</strong> all the historic sights of Haarlem</h2>
+          <p>The historical inner city of Haarlem features lots of extraordinary monuments. 
+          Discover the many interesting and surprising monuments that this city makes unique through this tour. 
+          Our guides will take you along several monuments in the city of Haarlem as you can see below.
+          </p>
+        </div>
+        <div class="col-md-7">
+          <h4><strong>Check out our Schedule</strong></h4>
+          <button><strong>↓</strong></button>
+        </div>
+      </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
-
-    </div>
+  </div>
+  <div class="embed-responsive embed-responsive-16by9">
+    <iframe
+    class="embed-responsive-item"
+    src="https://www.google.com/maps/d/u/0/embed?mid=1xMakWcCkWRc-eW4IGAR_zDG1UttbB1k&ehbc=2E312F"
+    title="Landmark Route">
+    </iframe>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  </div>
   </body>
-</html>
