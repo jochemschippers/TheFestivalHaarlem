d599bb933d1032c0f4adfa191cb3ba4e5f7a0715
RamonKea-inh <687187@student.inholland.nl>
2023-03-21 13:20:38 +0100

    fixed creating a landmark

--- php-restapi-solution-master/php-restapi-solution-master/app/controllers/admincontroller.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/controllers/admincontroller.php
@@ -26,26 +26,22 @@ class AdminController extends Controller {
     }
 
     public function getLandmark(){
-        return $this->landmarkService->getLandmark($_GET['landmarkID']);
+        return $this->landmarkService->getLandmark($_POST['landmarkID']);
     }
 
     public function createLandmark() {
         //$this->landmarkService->createLandmark($_POST['title'], $_POST['description'], $_POST['image']);
-        $landmark = new Landmark(null, null, null, null);
-        $landmark->setTitle($_POST['setTitle']);
-        $landmark->setDescription($_POST['setDescription']);
-        $landmark->setImage($_POST['setImage']);
+
+        $landmark = new Landmark(0, $_POST['setTitle'], $_POST['setDescription'], $_POST['setImage']);
         $this->landmarkService->createLandmark($landmark);
     }
 
     public function updateLandmark() {
         $this->landmarkService->updateLandmark($_POST['landmarkID'], $_POST['title'], $_POST['description'], $_POST['image']);
-        //header('Location: index.php?controller=admin&action=editLandmark');
     }
 
     public function deleteLandmark() {
         $this->landmarkService->deleteLandmark($_POST['landmarkID']);
-        //header('Location: index.php?controller=admin&action=deleteLandmark');
     }
 
 }
