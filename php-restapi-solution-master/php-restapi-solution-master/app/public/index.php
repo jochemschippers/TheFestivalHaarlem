3facbe62ead540812255974cd5a8350275f012d6
TayamEJ <89915786+Tayam0nline@users.noreply.github.com>
2023-03-31 21:02:38 +0200

    revert changes luuk push?

--- php-restapi-solution-master/php-restapi-solution-master/app/public/index.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/public/index.php
@@ -4,6 +4,6 @@ require __DIR__ . '/../patternrouter.php';
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 
 
-session_start();
+
 $router = new PatternRouter();
 $router->route($uri);
--- php-restapi-solution-master/php-restapi-solution-master/app/views/navbar.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/navbar.php
@@ -1,3 +1,4 @@
+
 <html lang="en">
 
 <head>
@@ -9,7 +10,31 @@
   <link href="/css/footer.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link href="../css/main.css" rel="stylesheet">
+
   <script src="https://kit.fontawesome.com/384ef59d1a.js" crossorigin="anonymous"></script>
+  <script>
+    const params = window.location.pathname.split("/");
+
+    function setStyle(foldername, styleName) {
+      var style = document.createElement('link');
+      style.setAttribute("rel", "stylesheet");
+      style.setAttribute("type", "text/css");
+      if (params[1] != "") {
+        style.setAttribute("href", "../css/" + foldername + "/" + styleName + ".css");
+      } else {
+        style.setAttribute("href", "../css/home.css");
+      }
+      document.head.appendChild(style);
+    }
+    var parameter = params[1].replace("-", "");  
+    setStyle(parameter, parameter);
+    if (params[2] != "") {
+      setStyle(params[1], "detailPage")
+    }
+  </script>
+
+
+
 </head>
 
 <header class="header-container">
@@ -18,18 +43,22 @@
   </div>
   <nav class="navbar" id="navbar">
     <span class="nav-item"><a href="/">Home</a></span>
-    <?php try { ?>
-      <?php foreach ($events as $event) { ?>
-        <span class="nav-item"><a href="/<?= str_replace(' ', '-', $event->getEventName()) ?>"><?= $event->getEventName() ?></a></span>
-      <?php } ?>
-    <?php } catch (error $e) { ?>
-      something went wrong while loading the navigation! Please try again later
-    <?php } ?>
+    <?php
+    try {
+      foreach ($events as $event) {
+        echo '<span class="nav-item"><a href="/' . str_replace(' ', '-', $event->getEventName()) . '">' . $event->getEventName()  . '</a></span>';
+      }
+    } catch (error $e) {
+      echo "something went wrong while loading the navigation! Please try again later";
+      echo "<script>console.log('Debug Objects: " . $e->getMessage() . "' );</script>";
+    }
+
+    ?>
+
   </nav>
   <div class="user-options">
-    <div class="option"><a href="/admin"><i class="fa fa-sharp fa-solid fa-screwdriver-wrench fa-2x"></i></a></div>
+    <div class="option"><a href="/admin"><i class="fa fa-user fa-2x"></i></div>
     <div class="option"><a href="/paymentpage"><i class="fa fa-shopping-cart fa-2x"></i></a></div>
-    <?php if (!isset($_SESSION['userID'])) { ?><div class="option"><a href="/account"><i class="fa fa-user fa-2x"></i></a></div><?php } else { ?>
-      <div class="option"><a href="logout.php"><i class="fa fa-right-from-bracket fa-2x"></i></a></div><?php }  ?>
+    <div class="option"><i class="fa fa-sharp fa-solid fa-screwdriver-wrench fa-2x"></i></div>
   </div>
 </header>
