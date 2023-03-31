824eff7d3c765d3e08289e7716279f289e3d322e
LuukBakkum <luuk.bakkum2000@gmail.com>
2023-03-31 20:50:06 +0200

    update 31-3 20:00

--- php-restapi-solution-master/php-restapi-solution-master/app/views/yummy/restaurant.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/yummy/restaurant.php
@@ -289,157 +289,158 @@
     <div class="overlay-container">
         <div class="overlay">
             <div class="container" id="overlay">
-                <?php $numberButtons = 1; ?>
-                <div class="row text-center">
-                    HIER LOOP VOOR EVENT ID EN ACTIVITY ID
-                    <h1>Make a reservation for:
-                        <?= $restaurant[0]->getRestaurantName() ?>
-                    </h1>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <h2>Select your arrival date and time</h2>
+                <form>
+                    <?php $numberButtons = 1; ?>
+                    <div class="row text-center">
+                        HIER LOOP VOOR EVENT ID EN ACTIVITY ID
+                        <h1>Make a reservation for:
+                            <?= $restaurant[0]->getRestaurantName() ?>
+                        </h1>
                     </div>
-                    <div class="col-md-6">
-                        <h2>Your reservation details</h2>
-                    </div>
-                </div>
-                <div class="row">
-                    <!-- <div class="btn-group" role="group" aria-label="Basic radio toggle button group"> -->
-                    <div class="col-md-3">
-                        <h4><ins>Thursday 26 July</ins></h4>
-                        <?php
-                        foreach ($session_times as $time) { ?>
-                            <!-- Display the session date and time with the session number -->
-                            <input type="radio" class="btn-check" name="btnradio" id="btnradio<?= $numberButtons ?>"
-                                autocomplete="off">
-                            <label class="btn btn-outline-primary w-100" for="btnradio<?= $numberButtons ?>"><?= "<b>Session $i: " . $time . "</b>" ?></label><br>
-
-                            <!-- Increase the start date and time by 1.5 hours for the next session -->
-                            <?php $numberButtons++;
-                        } ?>
-
-                        <!-- <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-                        <label class="btn btn-outline-primary w-100" for="btnradio2">Radio 2</label><br>
-
-                        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-                        <label class="btn btn-outline-primary w-100" for="btnradio3">Radio 3</label> -->
-                    </div>
-                    <div class="col-md-3">
-                        <h4><ins>Friday 27 July</ins></h4>
-                        <?php
-                        foreach ($session_times as $time) { ?>
-                            <!-- Display the session date and time with the session number -->
-                            <input type="radio" class="btn-check" name="btnradio" id="btnradio<?= $numberButtons ?>"
-                                autocomplete="off">
-                            <label class="btn btn-outline-primary w-100" for="btnradio<?= $numberButtons ?>"><?= "<b>Session $i: " . $time . "</b>" ?></label><br>
-
-                            <!-- Increase the start date and time by 1.5 hours for the next session -->
-                            <?php $numberButtons++;
-                        } ?>
-                    </div>
-                    <div class="col-md-4">
-                        <h4>Name on reservation</h4>
-                        <input class="form-control" type="text" placeholder="Enter name"
-                            aria-label="default input example">
-                    </div>
-                    <div class="col-md-2">
-                        <h4>Phone number</h4>
-                        <input class="form-control" id="phoneNr" type="number" placeholder="00 123456789"
-                            aria-label="default input example">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                    </div>
-                    <div class="col-md-6">
-                        <h2>Group size</h2>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-3">
-                        <h4><ins>Saturday 28 July</ins></h4>
-                        <?php
-                        foreach ($session_times as $time) { ?>
-                            <!-- Display the session date and time with the session number -->
-                            <input type="radio" class="btn-check" name="btnradio" id="btnradio<?= $numberButtons ?>"
-                                autocomplete="off">
-                            <label class="btn btn-outline-primary w-100" for="btnradio<?= $numberButtons ?>"><?= "<b>Session $i: " . $time . "</b>" ?></label><br>
-
-                            <!-- Increase the start date and time by 1.5 hours for the next session -->
-                            <?php $numberButtons++;
-                        } ?>
-                    </div>
-                    <div class="col-md-3">
-                        <h4><ins>Sunday 29 July</ins></h4>
-                        <?php
-                        foreach ($session_times as $time) { ?>
-                            <!-- Display the session date and time with the session number -->
-                            <input type="radio" class="btn-check" name="btnradio" id="btnradio<?= $numberButtons ?>"
-                                autocomplete="off">
-                            <label class="btn btn-outline-primary w-100" for="btnradio<?= $numberButtons ?>"><?= "<b>Session $i: " . $time . "</b>" ?></label><br>
-
-                            <!-- Increase the start date and time by 1.5 hours for the next session -->
-                            <?php $numberButtons++;
-                        } ?>
-                    </div>
-                    <div class="col-6">
-                        <h4>Number of adults</h4>
-
-                        <div class="input-group">
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary Aminus-btn" type="button">-</button>
-                            </div>
-                            <input type="number" id="nrAdult" value="1" min="1" max="20">
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary Aplus-btn" type="button">+</button>
-                            </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h2>Select your arrival date and time</h2>
                         </div>
-
-                        <h4>Number of children (-12)</h4>
-
-                        <div class="input-group">
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary Cminus-btn" type="button">-</button>
-                            </div>
-                            <input type="number" id="nrChild" value="0" min="0" max="20">
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary Cplus-btn" type="button">+</button>
-                            </div>
+                        <div class="col-md-6">
+                            <h2>Your reservation details</h2>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <br><br>
-                        <h4>Any special requests or allergies? Enter them below.</h4>
-                    </div>
-                    <div class="col-md-6">
-                        <br><br>
-                        <h4>Details</h4>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="mb-3">
-                            <textarea class="form-control" id="textArea" rows="3"></textarea>
+                    <div class="row">
+                        <!-- <div class="btn-group" role="group" aria-label="Basic radio toggle button group"> -->
+                        <div class="col-md-3">
+                            <h4><ins>Thursday 26 July</ins></h4>
+                            <?php $i = 1;
+                            foreach ($session_times as $time) { ?>
+                                <!-- Display the session date and time with the session number -->
+                                <input type="radio" class="btn-check" name="btnradio" id="btnradio<?= $numberButtons ?>"
+                                    autocomplete="off">
+                                <label class="btn btn-outline-primary w-100" for="btnradio<?= $numberButtons ?>"><?= "<b>Session $i: " . $time . "</b>" ?></label><br>
+
+                                <!-- Increase the start date and time by 1.5 hours for the next session -->
+                                <?php $numberButtons++;
+                                $i++;
+                            } ?>
+
+                        </div>
+                        <div class="col-md-3">
+                            <h4><ins>Friday 27 July</ins></h4>
+                            <?php $i = 1;
+                            foreach ($session_times as $time) { ?>
+                                <!-- Display the session date and time with the session number -->
+                                <input type="radio" class="btn-check" name="btnradio" id="btnradio<?= $numberButtons ?>"
+                                    autocomplete="off">
+                                <label class="btn btn-outline-primary w-100" for="btnradio<?= $numberButtons ?>"><?= "<b>Session $i: " . $time . "</b>" ?></label><br>
+
+                                <!-- Increase the start date and time by 1.5 hours for the next session -->
+                                <?php $numberButtons++;
+                                $i++;
+                            } ?>
+                        </div>
+                        <div class="col-md-4">
+                            <h4>Name on reservation</h4>
+                            <input class="form-control" id="customerName" type="text" placeholder="Enter name"
+                                aria-label="default input example">
+                        </div>
+                        <div class="col-md-2">
+                            <h4>Phone number</h4>
+                            <input class="form-control" id="phoneNr" type="number" placeholder="00 123456789"
+                                aria-label="default input example">
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="border" id="overview">
-                            <p>Number of adults: <span id="adults">1</span></p>
-                            <p>Number Children: <span id="children">0</span></p>
-                            <p>Reservation fee* (<span id="group">1</span>): </p>
-                            <hr>
-                            <p>Total price: </p>
+                    <div class="row">
+                        <div class="col-md-6">
                         </div>
-                        <p>*A reservation fee of €10,- pp. will be administerred.<br>
-                            This fee will be deducted from the final check on visiting the restaurant.
-                        </p>
-                        <input class="btn btn-primary" type="submit" value="Continue">
-                        <button type="button" onclick="hideOverlay();" class="btn btn-danger">Cancel</button>
+                        <div class="col-md-6">
+                            <h2>Group size</h2>
+                        </div>
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h4><ins>Saturday 28 July</ins></h4>
+                            <?php $i = 1;
+                            foreach ($session_times as $time) { ?>
+                                <!-- Display the session date and time with the session number -->
+                                <input type="radio" class="btn-check" name="btnradio" id="btnradio<?= $numberButtons ?>"
+                                    autocomplete="off">
+                                <label class="btn btn-outline-primary w-100" for="btnradio<?= $numberButtons ?>"><?= "<b>Session $i: " . $time . "</b>" ?></label><br>
+
+                                <!-- Increase the start date and time by 1.5 hours for the next session -->
+                                <?php $numberButtons++;
+                                $i++;
+                            } ?>
+                        </div>
+                        <div class="col-md-3">
+                            <h4><ins>Sunday 29 July</ins></h4>
+                            <?php $i = 1;
+                            foreach ($session_times as $time) { ?>
+                                <!-- Display the session date and time with the session number -->
+                                <input type="radio" class="btn-check" name="btnradio" id="btnradio<?= $numberButtons ?>"
+                                    autocomplete="off">
+                                <label class="btn btn-outline-primary w-100" for="btnradio<?= $numberButtons ?>"><?= "<b>Session $i: " . $time . "</b>" ?></label><br>
+
+                                <!-- Increase the start date and time by 1.5 hours for the next session -->
+                                <?php $numberButtons++;
+                                $i++;
+                            } ?>
+                        </div>
+                        <div class="col-6">
+                            <h4>Number of adults</h4>
+
+                            <div class="input-group">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary Aminus-btn" type="button">-</button>
+                                </div>
+                                <input type="number" id="nrAdult" value="1" min="1" max="20">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary Aplus-btn" type="button">+</button>
+                                </div>
+                            </div>
+
+                            <h4>Number of children (-12)</h4>
+
+                            <div class="input-group">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary Cminus-btn" type="button">-</button>
+                                </div>
+                                <input type="number" id="nrChild" value="0" min="0" max="20">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary Cplus-btn" type="button">+</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <br><br>
+                            <h4>Any special requests or allergies? Enter them below.</h4>
+                        </div>
+                        <div class="col-md-6">
+                            <br><br>
+                            <h4>Details</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                                <textarea class="form-control" id="textArea" rows="3"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="border" id="overview">
+                                <p>Number of adults: <span id="adults">1</span></p>
+                                <p>Number Children: <span id="children">0</span></p>
+                                <p>Reservation fee* (<span id="group">1</span>): </p>
+                                <hr>
+                                <p>Total price: </p>
+                            </div>
+                            <p>*A reservation fee of €10,- pp. will be administerred.<br>
+                                This fee will be deducted from the final check on visiting the restaurant.
+                            </p>
+                            <button class="btn btn-primary" type="submit">Continue</button>
+                            <button type="button" onclick="hideOverlay();" class="btn btn-danger">Cancel</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
