f5e31a40fa506e276344cee09f4def454f5f5b1b
LuukBakkum <luuk.bakkum2000@gmail.com>
2023-04-10 11:12:27 +0200

    added yummy functionality that will be used once tayam merched

--- php-restapi-solution-master/php-restapi-solution-master/app/controllers/testcontroller.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/controllers/testcontroller.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/controller.php';
 require_once __DIR__ . '/../services/jazzservice.php';
+require_once __DIR__ . '/../services/yummyservice.php';
 
 
 class TestController extends Controller
@@ -21,6 +22,17 @@ class TestController extends Controller
         $models = [
             "artists" => $this->service->getAllArtists(),
             "locations" => $this->service->getAllLocations(),
+        ];
+        $this->displayView($models);
+        include __DIR__ . '/../views/test/adminnav.php';
+    }
+    public function yummy(){
+        $this->service = new JazzService();
+        $models = [
+            "restaurants" => $this->yummyService->getAll(),
+            "menuItems" => $this->yummyService->getAllMenuItems(),
+            "images" => $this->yummyService->getAllImages(),
+            "restaurantFoodTypes" => $this->yummyService->getAllRestaurantFoodTypes(),
         ];
         $this->displayView($models);
         include __DIR__ . '/../views/test/adminnav.php';
