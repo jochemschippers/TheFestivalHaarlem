9f04ddaa950b789e0aa779d681f4fe5580bbc032
TayamEJ <89915786+Tayam0nline@users.noreply.github.com>
2023-04-10 13:16:47 +0200

    removed unnessecary code

--- php-restapi-solution-master/php-restapi-solution-master/app/views/test/index.php
+++ php-restapi-solution-master/php-restapi-solution-master/app/views/test/index.php
@@ -71,27 +71,6 @@
 
 //here could be possible php code, where if the count of $this->events is higher than 3, make dynamically new cards
 ?>
-  <section class="panel important">
-    <h2>Write Some News</h2>
-    <ul>
-      <li>Information Panel</li>
-    </ul>
-  </section>
-  
-  <section class="panel important">
-    <h2>Write a post</h2>
-      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <div class="twothirds">
-          Blog title:<br/>
-          <input type="text" name="title" size="40"/><br/><br/>
-          Content:<br/>    
-          <textarea name="newstext" rows="15" cols="67"></textarea><br/>  
-        </div>
-        <div>
-          <input type="submit" name="submit" value="Save" />
-        </div>
-        </div>
-      </form>
-  </section>
+
 
 </main>
