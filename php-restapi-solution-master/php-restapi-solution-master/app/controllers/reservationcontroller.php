0fd659d8d9dac41791a77732fbae8091443d4634
LuukBakkum <luuk.bakkum2000@gmail.com>
2023-06-22 17:02:35 +0200

    got rid of unused files (mine no worries)

--- php-restapi-solution-master/php-restapi-solution-master/app/controllers/reservationcontroller.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/controller.php';
-
-class ReservationController extends Controller {
-    public function index() {
-        require __DIR__ . '/../views/reservation/index.php';
-    }    
-}
-?>
